634ec24f573d3b42d3c55fc5e23c7a66fb1e86a5
Pietro Dell'Isola <dellisola.pietro@gmail.com>
2020-03-27 17:33:27 +0100

    first commit

---
+++ src/main/java/com/ap/SPRlibrary/SpRlibraryApplication.java
@@ -0,0 +1,13 @@
+package com.ap.SPRlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpRlibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpRlibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ap/SPRlibrary/SpRlibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ap.SPRlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpRlibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
